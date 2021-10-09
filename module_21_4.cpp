d9f4b61b99f002db9ceccab58b093edad9553d45
korshunov80@mail.ru <korshunov80@mail.ru>
2021-10-09 13:52:18 +0300

    Update module_21_4.cpp
    printScreen OK && input move

--- module_21_4.cpp
+++ module_21_4.cpp
@@ -18,13 +18,13 @@ struct Character
     Coordinates coordinate;
 };
 
-void inputCoordinates(Character coord[], int i)
+void inputCoordinates(Character enemy[], int i)
 {
-    coord[i].coordinate.x = std::rand() % 40;
-    coord[i].coordinate.y = std::rand() % 40;
+    enemy[i].coordinate.x = std::rand() % 40;
+    enemy[i].coordinate.y = std::rand() % 40;
 }
 
-void inputEnemy(Character enemy[])
+void inputEnemy(Character enemy[], Character &player)
 {
     for(int i = 0; i < 5; ++i)
     {
@@ -37,8 +37,10 @@ void inputEnemy(Character enemy[])
         {
             for (int j = 0; j < i; ++j)
             {
-                if(enemy[i].coordinate.x == enemy[j].coordinate.x ||
-                        enemy[i].coordinate.y == enemy[j].coordinate.y)
+                if((enemy[i].coordinate.x == enemy[j].coordinate.x &&
+                        enemy[i].coordinate.y == enemy[j].coordinate.y) &&
+                        (enemy[i].coordinate.x == player.coordinate.x &&
+                         enemy[i].coordinate.x == player.coordinate.x))
                 {
                     inputCoordinates(enemy, i);
                     j = 0;
@@ -59,31 +61,65 @@ void inputPlayer(Character &player)
     std::cin >> player.armor;
     std::cout << "Enter your damage: ";
     std::cin >> player.damage;
+    player.coordinate.x = 20;
+    player.coordinate.y = 20;
 }
 
-void printScreen(char field[][40], Character &enemy, Character &player)
+void printScreen(char field[][40], Character enemy[], Character &player)
 {
-    for(int i = 0; i < 40; ++i)
+    for(int i = 0; i < 5; ++i)
     {
-        for(int j = 0; j < 40; ++j)
+        std::cout << enemy[i].name << " " << enemy[i].life << " " << enemy[i].armor
+                  << " " <<enemy[i].damage << " " << enemy[i].whoIsIt << " x:"
+                  << enemy[i].coordinate.x << " y:" << enemy[i].coordinate.y << std::endl;
+    }
+    std::cout << player.name << " " << player.life << " " << player.armor
+              << " " << player.damage << " " << player.whoIsIt << " x:"
+              << player.coordinate.x << " y:" << player.coordinate.y << std::endl;
+    field[(player.coordinate.x)][(player.coordinate.y)] = 'P';
+
+    for(int i = 0; i < 5; ++i)
+    {
+        field[(enemy[i].coordinate.x)][(enemy[i].coordinate.y)] = 'E';
+    }
+
+    for(int j = 0; j < 40; ++j)
+    {
+        for(int i = 0; i < 40; ++i)
         {
+        if(field[i][j] != 'E' && field[i][j] != 'P') field[i][j] = '.';
+        std::cout << field[i][j];
 
         }
         std::cout << std::endl;
     }
 }
 int main() {
-    Character enemy[5];
-    Character player;
-    char field[40][40];
+    Character enemy[5]; // массив структуры врагов
+    Character player; // игрок
+    char field[40][40]{0}; // игровое поле
 
-    //inputPlayer(player);
-    inputEnemy(enemy);
-    for(int i = 0; i < 5; ++i)
+    //inputPlayer(player); // вводим данные игрока
+    player.name = "Ilya";
+    player.life = 500;
+    player.armor = 100;
+    player.damage = 50;
+    player.coordinate.x = 20;
+    player.coordinate.y = 20;
+
+    inputEnemy(enemy, player); // вводим данные врагов
+    printScreen(field, enemy, player); // выводим игровое поле на экран
+
+    std::string turn; // значение хода игрока
+    while(true)
     {
-        std::cout << enemy[i].name << " " << enemy[i].life << " " << enemy[i].armor
-                << " " <<enemy[i].damage << " " << enemy[i].whoIsIt << " x:"
-                << enemy[i].coordinate.x << " y:" << enemy[i].coordinate.y << std::endl;
+        std::cout << "Enter your move: ";
+        std::cin >> turn;
+        if(turn == "left") --player.coordinate.x;
+            else if (turn == "right") ++player.coordinate.x;
+            else if (turn == "down") --player.coordinate.y;
+            else if (turn == "up") ++player.coordinate.y;
+        else std::cout << "Input is not correct. Try again!!!\n";
+        fieldBoundaryCheck(); //проверка не выходит ли игрок за границы поля
     }
-
 }
