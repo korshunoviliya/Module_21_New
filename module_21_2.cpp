796b4c21cb4604476945197e9ac9a6ec2ddc00b3
korshunov80@mail.ru <korshunov80@mail.ru>
2021-10-10 15:29:02 +0300

    Update module_21_2.cpp
    replaced all arrays with vectors
    added enumeration of room types
    initialized the initial values of the resulting variables

--- module_21_2.cpp
+++ module_21_2.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+enum TYPE_ROOM
+        {
+                BEDROOM,
+                KITCHEN,
+                BATHROOM,
+                CHILDREN_ROOM,
+                LIVING_ROOM
+        };
 
 struct Bath
 {
-    double bathArea;
+    double bathArea{0};
     bool bathPipe{false};
 };
 
 struct Rooms
 {
-    std::string typeRoom{"default"};
+    TYPE_ROOM type_room;
     double roomArea;
 };
 
 struct Floors
 {
-    Rooms room[4];
+    std::vector <Rooms> room;
     double ceilingHeight;
     int numberRooms;
 };
@@ -25,25 +35,36 @@ struct House
     double houseArea;
     bool housePipe{false};
     int numberFloors;
-    Floors floor[3];
+    std::vector <Floors> floor;
 };
 
 struct Village
 {
-    int lotNumber;
-    int totalBuildings;
+    int lotNumber{0};
+    int totalBuildings{0};
     House house;
-    double garageArea;
-    double barnArea;
+    double garageArea{0};
+    double barnArea{0};
     Bath bath;
 };
 
+std::string typeRoom(int tr)
+{
+    if (tr == BEDROOM) return "bedroom";
+    else if (tr == KITCHEN) return "kitchen";
+    else if (tr == BATHROOM) return "bathroom";
+    else if (tr == CHILDREN_ROOM) return "children`s room";
+    else if (tr == LIVING_ROOM) return "living room";
+    else return "unknown room type";
+}
 
 int main() {
     int totalPlots;
     std::cout << "How many plots are there in the village?: ";
     std::cin >> totalPlots;
-    Village village[totalPlots];
+
+    std::vector <Village> village;
+    village.resize(totalPlots);
 
     for(int i = 0; i < totalPlots; ++i)
     {
@@ -91,31 +112,42 @@ int main() {
 
         std::cout << "Enter the number of floors in the house: ";
         std::cin >> village[i].house.numberFloors;
+
+        village[i].house.floor.resize(village[i].house.numberFloors);
+
         for (int j = 0; j < village[i].house.numberFloors; ++j)
         {
             std::cout << "What is the height of the ceilings on the floor: ";
             std::cin >> village[i].house.floor[j].ceilingHeight;
             std::cout << "How many rooms are on the floor #" << j + 1 << ": ";
             std::cin >> village[i].house.floor[j].numberRooms;
+
+            village[i].house.floor[j].room.resize(village[i].house.floor[j].numberRooms);
+            
             for (int k = 0; k < village[i].house.floor[j].numberRooms; ++k)
             {
-                std::cout << "Specify the type of numberRooms: ";
-                std::cin >> village[i].house.floor[j].room[k].typeRoom;
-                std::cout << "indicate the area of the " << village[i].house.floor[j].room[k].typeRoom << ": ";
+                int t_room;
+                std::cout << "Specify the type of rooms: ";
+                std::cout << "Input 0 for BEDROOM,\n"
+                             "Input 1 for KITCHEN,\n"
+                             "Input 2 for BATHROOM,\n"
+                             "Input 3 for CHILDREN`s ROOM,\n"
+                             "Input 4 for LIVING ROOM\n";
+                std::cin >> t_room;
+                village[i].house.floor[j].room[k].type_room = (TYPE_ROOM)t_room;
+                std::cout << "indicate the area of the " << typeRoom(t_room) << ": ";
                 std::cin >> village[i].house.floor[j].room[k].roomArea;
             }
         }
     }
-    double totalSquareRoom;
-    int totalPipe;
-    double totalSquare;
+    double totalSquareRoom{0};
+    int totalPipe{0};
+    double totalSquare{0};
 
     for(int i = 0; i < totalPlots; ++i)
     {
         totalPipe += village[i].bath.bathPipe + village[i].house.housePipe;
         totalSquare += village[i].house.houseArea;
-        std::cout << "Total pipes in the village: " << totalPipe << std::endl;
-        std::cout << "The area of all houses in the village: " << totalSquare << std::endl;
 
         for(int j = 0; j < village[i].house.numberFloors; ++j)
         {   for (int k = 0; k < village[i].house.floor[j].numberRooms; ++k)
@@ -123,7 +155,9 @@ int main() {
                 totalSquareRoom += village[i].house.floor[j].room[k].roomArea;
             }
         }
-        std::cout << "Total area of all rooms: " <<  totalSquareRoom << std::endl;
     }
+    std::cout << "Total pipes in the village: " << totalPipe << std::endl;
+    std::cout << "The area of all houses in the village: " << totalSquare << std::endl;
+    std::cout << "Total area of all rooms: " <<  totalSquareRoom << std::endl;
     return 0;
 }
